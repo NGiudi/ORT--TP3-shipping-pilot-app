30713ca4a1a92cb5339be777f2ea726d36a5e673
NGiudi <n.giudice@shipnow.com.ar>
2024-05-03 22:00:06 -0300

    update folder name

--- lib/models/index.dart
+++ lib/models/index.dart
@@ -1,7 +1,7 @@
 export 'package:shipping_pilot/models/address/address_model.dart';
 export 'package:shipping_pilot/models/client/client_model.dart';
-export 'package:shipping_pilot/models/travels/travel_model.dart';
-export 'package:shipping_pilot/models/travels/travel_stats_model.dart';
+export 'package:shipping_pilot/models/travel/travel_model.dart';
+export 'package:shipping_pilot/models/travel/travel_stats_model.dart';
 export 'package:shipping_pilot/models/user/user_model.dart';
 export 'package:shipping_pilot/models/vehicle/vehicle_model.dart';
 export 'package:shipping_pilot/models/visits/visit_model.dart';
