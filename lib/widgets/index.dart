a489b8bdbbd8d131b62ed8ee2860c98094ffc3b3
NGiudi <n.giudice@shipnow.com.ar>
2024-05-18 20:56:52 -0300

    relocation files

--- lib/widgets/index.dart
+++ lib/widgets/index.dart
@@ -1,6 +1,6 @@
 /* design system */
-export 'package:shipping_pilot/widgets/design_system/custom_badge.dart';
-export 'package:shipping_pilot/widgets/design_system/custm_divider.dart';
+export 'package:shipping_pilot/widgets/design_system/badges/custom_badge.dart';
+export 'package:shipping_pilot/widgets/design_system/dividers/custm_divider.dart';
 
 /* layout */
 export 'package:shipping_pilot/widgets/layout/appbar_widget.dart';
@@ -11,7 +11,7 @@ export 'package:shipping_pilot/widgets/layout/sidebar_widget.dart';
 export 'package:shipping_pilot/widgets/session/logout_alert_widget.dart';
 
 /* texts */
-export 'package:shipping_pilot/widgets/texts/section_title_widget.dart';
-export 'package:shipping_pilot/widgets/texts/field_widget.dart';
-export 'package:shipping_pilot/widgets/texts/text_widget.dart';
-export 'package:shipping_pilot/widgets/texts/title_widget.dart';
+export 'package:shipping_pilot/widgets/design_system/texts/section_title_widget.dart';
+export 'package:shipping_pilot/widgets/design_system/texts/field_widget.dart';
+export 'package:shipping_pilot/widgets/design_system/texts/text_widget.dart';
+export 'package:shipping_pilot/widgets/design_system/texts/title_widget.dart';
