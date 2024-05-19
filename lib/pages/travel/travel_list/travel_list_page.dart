55c5938c4602ce8e2b14d30310ebe7942c459121
NGiudi <n.giudice@shipnow.com.ar>
2024-05-18 22:28:22 -0300

    add title in travel list

--- lib/pages/travel/travel_list/travel_list_page.dart
+++ lib/pages/travel/travel_list/travel_list_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shipping_pilot/pages/travel/travel_list/widgets/travel_list_widget.dart';
 import 'package:shipping_pilot/providers/index.dart';
 
 import 'package:shipping_pilot/widgets/index.dart';
@@ -28,28 +28,15 @@ class TravelListPage extends ConsumerWidget {
     return Scaffold(
       drawer: const SidebarWidget(),
       appBar: const AppbarWidget(),
-      body: ListView.builder(
-          itemCount: travels.length,
-          itemBuilder: (context, idx) {
-            Travel travel = travels[idx];
-
-            return ListTile(
-              onTap: () {
-                context.push('/travel_detail/${travel.id}');
-              },
-              subtitle: Text('Conductor: ${travel.driver.fullName()}'),
-              title: Row(
-                children: [
-                  const Icon(Icons.fire_truck),
-                  const SizedBox(width: 8),
-                  Text(travel.getDateOfId()),
-                  const SizedBox(width: 8),
-                  TravelStatusBadgeWidget(status: travel.status)
-                ],
-              ),
-              trailing: const Icon(Icons.chevron_right),
-            );
-          }),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: ListView(
+          children: [
+            const SectionTitleWidget(text: 'Recorridos'),
+            TravelListWidget(travels: travels),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/pages/travel/travel_list/widgets/travel_list_widget.dart
+++ lib/pages/travel/travel_list/widgets/travel_list_widget.dart
@@ -10,36 +10,29 @@ class TravelListWidget extends StatelessWidget {
 
   final List<Travel> travels;
 
-  const TravelListWidget({super.key, required this.travels});
+  const TravelListWidget({ super.key, required this.travels });
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: const SidebarWidget(),
-      appBar: const AppbarWidget(),
-      body: ListView.builder(
-        itemCount: travels.length,
-        itemBuilder: (context, idx) {
-          Travel travel = travels[idx];
-
-          return ListTile(
-            onTap: () {
-              context.push('/travel_detail/${travel.id}');
-            },
-            subtitle: Text('Conductor: ${travel.driver.fullName()}'),
-            title: Row(
-              children: [
-                const Icon(Icons.fire_truck),
-                const SizedBox(width: 8),
-                Text(travel.getDateOfId()),
-                const SizedBox(width: 8),
-                TravelStatusBadgeWidget(status: travel.status)
-              ],
-            ),
-            trailing: const Icon(Icons.chevron_right),
-          );
-        },
-      ),
+    return Column(
+      children: travels.map((Travel travel) {
+        return ListTile(
+          onTap: () {
+            context.push('/travel_detail/${travel.id}');
+          },
+          subtitle: Text('Conductor: ${travel.driver.fullName()}'),
+          title: Row(
+            children: [
+              const Icon(Icons.fire_truck),
+              const SizedBox(width: 8),
+              Text(travel.getDateOfId()),
+              const SizedBox(width: 8),
+              TravelStatusBadgeWidget(status: travel.status)
+            ],
+          ),
+          trailing: const Icon(Icons.chevron_right),
+        );
+      }).toList(),
     );
   }
 }
